993258679b56c50f1954a59a0599324b948782d5
shinshilkina <anastasiashilina98@gmail.com>
2020-11-15 22:42:29 +0700

    #1.1 made data range inputs with calendar

--- src/components/elements/Datarange.vue
+++ src/components/elements/Datarange.vue
@@ -2,37 +2,67 @@
   <div class="datarange">
     <div class="dropdown__date">
       <div class="dropdown__date__title">прибытие</div>
-      <input class="dropdown__date__input arrived" placeholder="ДД.ММ.ГГГГ">
+      <input class="dropdown__date__input arrived" placeholder="ДД.ММ.ГГГГ" ref="arrived">
       <img class="dropdown__date__expand" src="../../assets/expand_more.png">
     </div>
     <div class="dropdown__date">
       <div class="dropdown__date__title">выезд</div>
-      <input  class="dropdown__date__input departure" placeholder="ДД.ММ.ГГГГ">
+      <input  class="dropdown__date__input departure" placeholder="ДД.ММ.ГГГГ" ref="departure">
       <img class="dropdown__date__expand" src="../../assets/expand_more.png">
     </div>
   </div>
 </template>
 
 <script>
-
+import Lightpick from 'lightpick';
 export default {
   name: "Datarange.vue",
   components: {
 
   },
   data() {
+    let picker;
     return {
-
+      picker
     };
   },
-  methods: {
+  mounted() {
+    this.picker = new Lightpick({
+      field: this.$refs.arrived,
+      secondField: this.$refs.departure,
+      singleDate: false,
+      lang: 'ru',
+      locale: {
+        buttons: {
+          reset: 'очистить',
+          apply: 'применить'
+        },
+        tooltip: {
+          one: 'день',
+          few: 'дня',
+          many: 'дней',
+        },
+        pluralize: function(i, locale) {
+          if ('one' in locale && i % 10 === 1 && !(i % 100 === 11)) return locale.one;
+          if ('few' in locale && i % 10 === Math.floor(i % 10) && i % 10 >= 2 && i % 10 <= 4 && !(i % 100 >= 12 && i % 100 <= 14)) return locale.few;
+          if ('many' in locale && (i % 10 === 0 || i % 10 === Math.floor(i % 10) && i % 10 >= 5 && i % 10 <= 9 || i % 100 === Math.floor(i % 100) && i % 100 >= 11 && i % 100 <= 14)) return locale.many;
+          if ('other' in locale) return locale.other;
 
+          return '';
+        }
+      },
+      footer: true
+    });
+  },
+  beforeDestroy() {
+    this.picker.destroy();
   }
 }
 </script>
 
 <style lang="scss">
 @import "../style";
+@import "calendar";
 
 .dropdown__date {
   position: relative;
